0d0477621fde4b5d128a60e28ff4f6d121a92a93
xquimoh <michellemorikawa@hotmail.com>
2023-03-16 22:34:20 -0300

    init commit

---
+++ Exercicios/Exercicios1a7.sql
@@ -0,0 +1,35 @@
+--CREATE DATABASE Exercicios
+
+--USE Exercicios
+
+--Exercício 1
+--CREATE TABLE Clientes(
+	--ID INT PRIMARY KEY,
+	--NOME VARCHAR(20),
+	--TELEFONE VARCHAR(20),
+	--ENDERECO VARCHAR(50)
+--)
+
+--Exercício 2
+--INSERT INTO Clientes (ID, NOME, TELEFONE, ENDERECO) VALUES (1, 'Vinicius Silva', '987654', 'Rua Girassol'),
+														  --(2, 'Maria Antonia', '123456', 'Rua Rosas'),
+														  --(3, 'Marcus Vinicius', '654123', 'Rua Itajai')
+
+--Exercício 3
+--SELECT * FROM Clientes
+
+--Exercício 4
+--SELECT ID FROM Clientes
+
+--Exercício 5
+--SELECT * FROM Clientes
+--WHERE ENDERECO like '%g%'
+
+--Exercício 6
+--UPDATE Clientes
+--SET ENDERECO = 'Rua do Limão'
+--WHERE NOME = 'Marcus Vinicius'
+
+--Exercício 7
+--DELETE Clientes
+--WHERE ID = 2
